d055c520d716484ec6d584a1b7175cf56396d5b1
Jeppe Joergensen <jeppe@ejvinds.dk>
2014-11-23 23:10:34 -0800

    Added more syso

--- SUM projekt - Kryptering/src/Main.java
+++ SUM projekt - Kryptering/src/Main.java
@@ -3,6 +3,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("En lille test udskrift til jer dejlige drenge! <3");
+		System.out.println("Her er en ekstra lille udskrift til jer.");
 	}
 
 }
